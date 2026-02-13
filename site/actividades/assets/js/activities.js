bf7ad57913b0bfa1074b6e8cd9a6046618c4d40b
L1911 <luadepanches@gmail.com>
2026-02-13 19:18:35 +0100

    Actualizada Feira Investigación

--- site/actividades/assets/js/activities.js
+++ site/actividades/assets/js/activities.js
@@ -1,30 +1,19 @@
 const NEXT_ACTIVITIES = [
     [
-        "Visita ao CITEEC",
-        "Que se fai nun centro de investigación? Non todo é o mundo da empresa, descubre nesta visita a outra cara da moeda! Inscríbete xa!",
-        "3, 5 Febreiro 2026",
-        "/actividades/assets/img/activities/actividad_visita_ao_citeec_2026.png",
-        "https://forms.office.com/e/r6QLBMVEmH"
-    ],
-    [
-        "Visita ao CITIC",
-        "Que se fai nun centro de investigación? Non todo é o mundo da empresa, descubre nesta visita a outra cara da moeda! Inscríbete xa!",
-        "11, 12 Febreiro 2026",
-        "/actividades/assets/img/activities/actividad_visita_ao_citic_2026.png",
-        "https://forms.office.com/e/KbGhtyzVBr"
-    ],
-    [
         "Torneo LoL",
         "Saca o teu lado gamer e álzate coa victoria no noso torneo de LoL!",
         "Febreiro 2026 (en curso)",
         "/actividades/assets/img/activities/actividad_torneo_lol_2026.png"
     ],
+
     [
         "Feira de investigación",
         "Coñece os grupos de investigación asociados á FIC e as súas liñas de traballo!",
-        "Marzo 2026",
-        "/actividades/assets/img/activities/actividad_feira_investigacion.png"
+        "3, 4, 5 Marzo 2026",
+        "/actividades/assets/img/activities/actividad_feira_investigacion.png",
+        "/actividades/assets/img/poster/cartel_feira_investigacion.png"
     ],
+    
     [
         "Charla Markdown",
         "Próximamente un dos socios de DAFIC impartirá unha charla sobre Markdown da man do GPUL",
@@ -56,6 +45,22 @@ const NEXT_ACTIVITIES = [
 ]
 
 const PREVIOUS_ACTIVITIES = [
+    [
+        "Visita ao CITIC",
+        "Que se fai nun centro de investigación? Non todo é o mundo da empresa, descubre nesta visita a outra cara da moeda! Inscríbete xa!",
+        "11, 12 Febreiro 2026",
+        "/actividades/assets/img/activities/actividad_visita_ao_citic_2026.png",
+        "https://forms.office.com/e/KbGhtyzVBr"
+    ],
+
+    [
+        "Visita ao CITEEC",
+        "Que se fai nun centro de investigación? Non todo é o mundo da empresa, descubre nesta visita a outra cara da moeda! Inscríbete xa!",
+        "3, 5 Febreiro 2026",
+        "/actividades/assets/img/activities/actividad_visita_ao_citeec_2026.png",
+        "https://forms.office.com/e/r6QLBMVEmH"
+    ],
+
     [
         "Partidas D&D",
         "Non te perdas esta oportunidade de pasar unha boa tarde xogando a D&D!",
