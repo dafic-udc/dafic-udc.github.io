b8dbe8761f1ab58cbb0b3533c123d46b355d0fc3
danielfeitopin <danielfeitopin@protonmail.com>
2026-04-16 19:51:28 +0200

    refactor: Modificado o script de protección de email
    Utilizado correo en base64 en atributos data-e

--- site/assets/js/protect_email.js
+++ site/assets/js/protect_email.js
@@ -2,14 +2,5 @@
 
 // Retrieve email links with class 'email' and set their href attributes
 document.querySelectorAll('a.email').forEach(emailLink => {
-
-    // Join inner spans
-    const spans = emailLink.querySelectorAll('span');
-    email = '';
-    spans.forEach(span => {
-        email += span.innerText;
-    });
-
-    // Set mailto link
-    emailLink.href = `mailto:${email}`;
+    emailLink.href = `mailto:${atob(emailLink.getAttribute("data-e"))}`;
 });
